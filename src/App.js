6062eb65d35e5ed7a7ee8dd633030336f969cfad
Olawoyin <yustee2017@gmail.com>
2024-08-13 15:16:34 +0100

    All files deleted - This react file will now be used as a practices

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Main from './components/Main';
-import api from './api/config';
-import Card from './components/Card';
-import Footer from './components/Footer';
 
 
 
 function App() {
-
-
-  const [movieList , setMovieList] = useState([]);
-  const [searchMovieList , setSearchMovieList] = useState([]);
-  
-  const fetchData = async() => {
-    try {
-      const users = await axios.get(`https://api.themoviedb.org/3/discover/movie`,{
-        params: {
-          api_key: api.apiKey
-        }
-      });
-      const data = users.data.results;
-      setMovieList(data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  useEffect(()=>{
-    fetchData();
-  },[])
-
-  
-
-
   return (
     <>
-     <Main
-        movieList={movieList}
-        setMovieList={setMovieList}
-     />
-     <div className='container mt-5'>
-      <div className='d-flex align-items-center justify-content-between'>
-        <h2 className='fw-bolder'>Featured Movies</h2>
-        <a href='#'>See more</a>
-      </div>
-      <Card 
-        movieList={movieList}
-        setMovieList={setMovieList}
-      />
-     </div>
-     <Footer />
+      hello World 
     </>
    
   );
--- src/api/config.js
+++
@@ -1,8 +0,0 @@
-const api = {
-    baseUrl: 'https://www.themoviedb.org/3/',
-    apiKey: 'c65bedcc868173c3a6b153eeabf66592',
-    // originalImage: (imgpath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    // w500: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
-
-export default api;
