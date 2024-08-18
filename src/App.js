1fb0caae1150a20cde665eaca7e9dfffe642dec3
Olawoyin <yustee2017@gmail.com>
2024-08-18 15:52:53 +0100

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import Header from './Components/Header.jsx';
-import Hero from './Components/Hero.jsx';
-import Footer from './Components/Footer.jsx';
+import Navbar from './Navbar/Navbar.jsx';
 
 
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero /> 
-      <Footer />
+      <Navbar />
     </>
    
   );
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { RiMailSendLine } from "react-icons/ri";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { FaXTwitter } from "react-icons/fa6";
+import { VscGithubAlt } from "react-icons/vsc";
+
+const Navbar = () => {
+  return (
+    <header>
+        <div className="container">
+            <div className="d-flex align-items-center header justify-content-between">
+                <div className="mail gap-2">
+                    <RiMailSendLine />
+                    <p>yustee2017@gmail.com</p>
+                </div>
+                
+                <div className="mail links">
+                    <ul className='d-flex gap-4 m-0'>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="#home">Home</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="#about">About</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="#skills">Skills</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="#portfolio">Portfolio</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="mail gap-3">
+                    <a href="#github">
+                        <VscGithubAlt />
+                    </a>
+                    <a href="#twitter">
+                        <FaXTwitter />
+                    </a>
+                    <a href="#linkedin">
+                        <SlSocialLinkedin />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
