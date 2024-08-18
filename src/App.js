7b680761ba18a6f1add549176e9b191a0deb6761
Olawoyin <yustee2017@gmail.com>
2024-08-18 12:42:16 +0100

    Starting my portfolio project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import Header from './Components/Header.jsx';
+import Hero from './Components/Hero.jsx';
+import Footer from './Components/Footer.jsx';
 
 
 
 function App() {
   return (
     <>
-      hello World 
+      <Header />
+      <Hero /> 
+      <Footer />
     </>
    
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-center py-2 mt-4'>
+        <small>c 2024 Olawoyin Gbolahan. All rights reserved</small>
+    </footer>
+  )
+}
+
+export default Footer
