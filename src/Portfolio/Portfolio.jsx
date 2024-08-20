70a16fe21d93070242da6b7868cdedac44c473b8
Olawoyin <yustee2017@gmail.com>
2024-08-21 00:29:28 +0100

    Add more button added to the portfolio

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { AiOutlineReload } from "react-icons/ai";
+import Project from './data';
 
 const Portfolio = () => {
+
+    const [visible, setVisible] = useState(3)
+
+
+    const handleVisible = () =>{
+      setVisible(prevState => prevState + 3)
+    }
   return (
     <div className='py-4' id='portfolio'>
       <div className="container2">
@@ -11,46 +20,33 @@ const Portfolio = () => {
             </div>
             <div className="grid1   m-0 ">
 
-
-              <div className='item2 bg-white shadow-sm'>
-                <div className="image">
-                  <img src="../../Images/Redmi1.jpeg" alt="" />
-                </div>
-                <div className="port-content d-flex flex-column gap-2 p-3">
-                  <h6 className='fw-bold'>Redmi Note 9s Control Panel</h6>
-                  <small  className='text-muted'>Lorem ipsum,iam aliquid sit fugit quibusdam, nihil distinctio id! Quibusdam?</small>
-                  <a href="#view">View <IoIosArrowRoundForward /></a>
-                </div>
-              </div>
-
-              <div className='item2 bg-white shadow-sm'>
-                <div className="image">
-                  <img src="../../Images/signup.png" alt="" />
-                </div>
-                <div className="port-content d-flex flex-column gap-2 p-3">
-                  <h6 className='fw-bold'>Redmi Note 9s Control Panel</h6>
-                  <small  className='text-muted'>Lorem ipsum,iam aliquid sit fugit quibusdam, nihil distinctio id! Quibusdam?</small>
-                  <a href="#view">View <IoIosArrowRoundForward /></a>
-                </div>
-              </div>
-
-              <div className='item2 bg-white shadow-sm'>
-                <div className="image">
-                  <img src="../../Images/signup2.png" alt="" />
-                </div>
-                <div className="port-content d-flex flex-column gap-2 p-3">
-                  <h6 className='fw-bold'>Redmi Note 9s Control Panel</h6>
-                  <small  className='text-muted'>Lorem ipsum,iam aliquid sit fugit quibusdam, nihil distinctio id! Quibusdam?</small>
-                  <a href="#view">View <IoIosArrowRoundForward /></a>
-                </div>
-              </div>
-
-
-              
+              {
+                Project.slice(0, visible).map((item) =>{
+                  return(
+                    <div key={item.id} className='box item2 bg-white shadow-sm'>
+                      <div className="image">
+                        <img src={item.image} alt="" />
+                      </div>
+                      <div className="port-content d-flex flex-column gap-2 p-3">
+                        <h6 className='fw-bold'>{item.name}</h6>
+                        <small  className='text-muted'>{item.desc}</small>
+                        <a href="#view">View <IoIosArrowRoundForward /></a>
+                      </div>
+                    </div>
+                  )
+                })
+              }
 
 
 
             </div>
+            <div className="sec-header my-4 text-center ">
+              <button onClick={handleVisible} className="works  mx-auto">
+                  <p>Load More</p>
+                  <AiOutlineReload size={20} />
+              </button>
+            </div>
+            
       </div>
     </div>
   )
--- src/Portfolio/data.js
+++ src/Portfolio/data.js
@@ -1,16 +1,103 @@
 const Project = [
     {
-        id: "1",
-        category: "React",
-        about: "Redmi Note 9s control panel created using HTML , CSS and JavaScript",
-        image: "redmi.png",
+        id: 1,
+        name: "Redmi Note 9s Control Panel",
+        link : "https://redmi9s.netlify.app/",
+        image: "../../Images/Redmi.jpeg",
+        desc: "Redmi Note 9s control panel craeted with HTML, CSS and JAVASCRIPT.",
+        language: ""
+        
+    },
+
+    {
+        id: 2,
+        name: "Component Signup",
+        link : "https://component-signup.netlify.app/",
+        image: "../../Images/Component-signup.png",
+        desc: "Frontend Mentor Challenge | Intro component with sign up form",
+        language: ""
     },
     {
-        id: "1",
-        category: "React",
-        about: "Redmi Note 9s control panel created using HTML , CSS and JavaScript",
-        image: "redmi.png",
-    }
+        id: 3,
+        name: "Four Cards",
+        link : "https://olawoyin-fourcards.netlify.app/",
+        image: "../../Images/Four-cards.png",
+        desc: "Frontend Mentor Challenge | Four card feature section",
+        language: ""
+    },
+    {
+        id: 4,
+        name: "Learning Website",
+        link : "https://frenchlearningweb.netlify.app/",
+        image: "../../Images/Learning-web.png",
+        desc:"French Learning website created using CSS Grid.",
+        language: ""
+    },
+    {
+        id: 5,
+        name: "Blog web",
+        link : "https://mozanbinova.netlify.app",
+        image: "../../Images/Blog.png",
+        desc: "Fashion and Lifestyle blog created with HTML , CSS and Javascript",
+        language: ""
+    },
+    {
+        id: 6,
+        name: "User Registration + CRUD",
+        link : "https://github.com/Olawoyin1/User-Registration---CRUD-Operation",
+        image: "../../Images/crud.png",
+        desc: "User registration + CRUD operation using PHP and mySQL",
+        language: ""
+    },
+    {
+        id: 7,
+        name: "Sign up form + Validation",
+        link : "https://user-reg-validation.netlify.app/",
+        image: "../../Images/Form.png",
+        desc: "User registration with Form validation using Javascript",
+        language: ""
+    },
+    {
+        id: 8,
+        name: "Counter App",
+        link : "https://olawoyin-counterapp.netlify.app/",
+        image: "../../Images/Counter-app.png",
+        desc: "Javascript Counter Application.",
+        language: ""
+    },
+    {
+        id: 9,
+        name: "Modal",
+        link : "https://olawoyin-modal.netlify.app/",
+        image: "../../Images/Modal.png",
+        desc: "Modal Popup | UI element that appears in the foreground of a website",
+        price: "$1,200.00",
+        language: ""
+    },
+    {
+        id: 10,
+        name: "U-secured web",
+        link : "Action",
+        image: "../../Images/U-secured.png",
+        desc: "Safety tech web application.",
+        language: ""
+    },
+    {
+        id: 11,
+        name: "Accordion",
+        link : "https://olawoyin-accordion.netlify.app/",
+        image: "../../Images/Accordion.png",
+        desc: "Useful when when you want to toggle between hiding and showing large amount of content.",
+        language: ""
+    },
+    {
+        id: 12,
+        name: "Profile Card",
+        link : "https://olawoyin-profile-card.netlify.app/",
+        image: "../../Images/Profile-card.png",
+        desc: "Frontend Mentor Challenge | Profile card component",
+        language: ""
+    },
 ]
 
 export default Project
